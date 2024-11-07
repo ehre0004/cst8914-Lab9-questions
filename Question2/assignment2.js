0eadfac0aef92438066d9b50e3c5db6be6a4a94b
ehre0004 <ehre0004@algonquinlive.com>
2024-11-07 12:44:34 -0500

    test validation

--- Question2/assignment2.js
+++ Question2/assignment2.js
@@ -15,9 +15,55 @@ if (submitBtn) {
 }
 
 // provides the code for setting a message to the aria live region
-function setStatus(msg) {
+function setStatus(msg, status) {
 	var statusDiv = document.getElementById("status");
-	statusDiv.innerHTML = "<p>"+msg+"</p>";
+	statusDiv.innerHTML = "<p class=\""+status+"\">"+msg+"</p>";
+}
+
+function getReadableName(id) {
+	switch (id) {
+		case 'name':
+			return 'your name';
+		case 'emailaddy':
+			return 'your email';
+		case 'comments':
+			return 'your comments';
+		default:
+			return null;
+	}
+}
+
+function printReadableList(list) {
+	var printableList = "";
+	let c = ", ";
+	for (let i = 0; i < list.length; i++) {
+		if (i != 0) {
+			printableList += c;
+		}
+		printableList += list[i];
+	}
+	return printableList;
+}
+
+function validateForm() {
+	var form = {
+		name: document.getElementById('name'),
+		email: document.getElementById('emailaddy'),
+		comments: document.getElementById('comments')
+	};
+	var isValid = false;
+	var list = [];
+	for (let x of form) {
+		if (x == null || x.value == "") {
+			isValid = false;
+			list.push(getReadableName(x.id))
+		} else {
+			isValid = true;
+		}
+	}
+	if (!isValid) {
+		setStatus('Error. Please enter a value for the following form fields:'+printReadableList(list),'error');
+	}
 }
 
 function sendComments (e) {
@@ -27,7 +73,7 @@ function sendComments (e) {
 	let msg = "Thank you for sending your comments. Your comments have already been thrown into the dustbin of the Interwebs, and will be ignored at once!"	;
 
 	// Now, add the code to insert the msg into the aria live region
-	setStatus(msg);
+	setStatus(msg, 'success');
 
 	
 } // End of sendComments
